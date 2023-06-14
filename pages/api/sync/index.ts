29bbb5f50fdf40b703c64af61e1f945ce6b59f02
brunoken22 <bruno_am_22@hotmail.com>
2023-06-14 15:14:02 -0300

    commit

--- pages/api/sync/index.ts
+++ pages/api/sync/index.ts
@@ -1,62 +1,66 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getOffsetAndLimitFomReq } from "lib/requests";
+import methods from "micro-method-router";
+
 import { index } from "lib/algolia";
 import { base } from "lib/airtable";
 import { off } from "process";
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
-   // const { finalLimit, finalOffset } = getOffsetAndLimitFomReq(req, 100, 3);
-   // const objetoPrueba = {
-   //    name: "bruno",
-   //    edad: "25",
-   //    objectID: "1",
-   // };
-   // index
-   //    .saveObjects([objetoPrueba])
-   //    .then((res) => {
-   //       console.log("Conectado correctamente" + res);
-   //    })
-   //    .catch((e) => {
-   //       console.log("Error");
-   //    });
+export default methods({
+   post(req: NextApiRequest, res: NextApiResponse) {
+      // const { finalLimit, finalOffset } = getOffsetAndLimitFomReq(req, 100, 3);
+      // const objetoPrueba = {
+      //    name: "bruno",
+      //    edad: "25",
+      //    objectID: "1",
+      // };
+      // index
+      //    .saveObjects([objetoPrueba])
+      //    .then((res) => {
+      //       console.log("Conectado correctamente" + res);
+      //    })
+      //    .catch((e) => {
+      //       console.log("Error");
+      //    });
 
-   base("Furniture")
-      .select({
-         pageSize: 10,
-      })
-      .eachPage(
-         async function (records, fetchNextPage) {
-            // records.forEach(function(record) {
-            //     console.log('Retrieved', record.get('Name'));
-            // });
-            // res.send({
-            //    results: records.map((item) => item.fields),
-            //    pagination: {
-            //       offset: finalOffset,
-            //       limit: finalLimit,
-            //       // total: lista.length,
-            //    },
-            // });
-            const object = records.map((r) => {
-               return {
-                  objectID: r.id,
-                  ...r.fields,
-               };
-            });
-            const respuestaAlgolia = await index.saveObjects(object);
-            console.log(respuestaAlgolia);
+      base("Furniture")
+         .select({
+            pageSize: 10,
+         })
+         .eachPage(
+            async function (records, fetchNextPage) {
+               // records.forEach(function(record) {
+               //     console.log('Retrieved', record.get('Name'));
+               // });
+               // res.send({
+               //    results: records.map((item) => item.fields),
+               //    pagination: {
+               //       offset: finalOffset,
+               //       limit: finalLimit,
+               //       // total: lista.length,
+               //    },
+               // });
+               const object = records.map((r) => {
+                  return {
+                     objectID: r.id,
+                     ...r.fields,
+                  };
+               });
+               const respuestaAlgolia = await index.saveObjects(object);
+               console.log(respuestaAlgolia);
 
-            fetchNextPage();
-         },
-         function done(err) {
-            if (err) {
-               console.error(err);
-               return;
+               fetchNextPage();
+            },
+            function done(err) {
+               if (err) {
+                  console.error(err);
+                  return;
+               }
+               console.log("Termino");
             }
-            console.log("Termino");
-         }
-      );
+         );
 
-   res.send("TERMINADO");
-   // const listasFinales = lista.slice(finalOffset, finalOffset + finalLimit);
-}
+      res.send("TERMINADO");
+      // const listasFinales = lista.slice(finalOffset, finalOffset + finalLimit);
+   },
+});
