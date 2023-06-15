6b110736a8799cde33d65733522701da121be26c
brunoken22 <bruno_am_22@hotmail.com>
2023-06-15 18:13:33 -0300

    commit

---
+++ pages/api/test/index.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import methods from "micro-method-router";
+
+export default methods({
+   get(req: NextApiRequest, res: NextApiResponse) {
+      res.send({
+         message: "todo ok.",
+      });
+   },
+});
